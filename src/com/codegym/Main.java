39f1744d9ec1e282ef7217a20b7c2c15da535f23
Nguyen Thanh Binh <kubap@MacBook-Pro-cua-KU.local>
2022-01-10 17:23:18 +0700

    done

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,54 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        khai bao bien choice luu gia tri chon lua.
+        int choice = -1;
+//        tao doi tuong input thuoc lop Scanner de nhap lieu.
+        Scanner sc = new Scanner(System.in);
+//        chung ta se can hien thi thong tin menu gom 3 lua chon.
+        while (choice != 0) {
+            System.out.println("Menu");
+            System.out.println("1. Draw the triangle");
+            System.out.println("2. Draw the square");
+            System.out.println("3. Draw the rectangle");
+            System.out.println("0. Exit");
+            System.out.println("Enter your choice: ");
+            choice = sc.nextInt();
+//        su dung cau truc switch case de cho phep nguoi dung xu ly cac tinh huong lua chon.
+            switch (choice) {
+                case 1:
+                    System.out.println("Draw the triangle");
+                    System.out.println("******");
+                    System.out.println("*****");
+                    System.out.println("****");
+                    System.out.println("***");
+                    System.out.println("**");
+                    System.out.println("*");
+                    break;
+                case 2:
+                    System.out.println("Draw the square");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    break;
+                case 3:
+                    System.out.println("Draw the rectangle");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("No Choice!");
+            }
+        }
+    }
+}
